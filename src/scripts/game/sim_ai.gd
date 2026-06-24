83156f017a06caafe02ea0758e25b3ee565833e9
JodgeMin <1460881525@qq.com>
2026-06-24 19:47:18 +0800

    Fix Sim AI lane distribution logic

--- src/scripts/game/sim_ai.gd
+++ src/scripts/game/sim_ai.gd
@@ -13,33 +13,33 @@ func generate(initial_a_pol : Magnet.Polarity, initial_b_pol : Magnet.Polarity, 
 	var current_x := start_world_x
 	var surf_a := Surface.FLOOR
 	var surf_b := Surface.FLOOR
-	var last_decision_lane := -1
+	var last_lane := -1
 
 	while current_x < start_world_x + length:
 		var swap_trigger := false
 		var mag_a := false
 		var mag_b := false
+		var chosen_lane := -1
 
-		var decision := rng.randi_range(0, 2)
-		if decision == 2:
+		var r := rng.randf()
+		if r < 0.2:
 			swap_trigger = true
-		elif decision == 0:
-			if last_decision_lane == 0 and rng.randi_range(0, 2) > 0:
-				decision = 1
+		elif r < 0.6:
+			chosen_lane = 0
+		else:
+			chosen_lane = 1
+
+		if chosen_lane == last_lane and chosen_lane >= 0:
+			chosen_lane = 1 - chosen_lane
+
+		if chosen_lane == 0:
 			surf_a = Surface.CEILING if surf_a == Surface.FLOOR else Surface.FLOOR
 			mag_a = true
-			last_decision_lane = 0
-		else:
-			if last_decision_lane == 1 and rng.randi_range(0, 2) > 0:
-				decision = 0
-				surf_a = Surface.CEILING if surf_a == Surface.FLOOR else Surface.FLOOR
-				mag_a = true
-				last_decision_lane = 0
-			else:
-				surf_b = Surface.CEILING if surf_b == Surface.FLOOR else Surface.FLOOR
-				mag_b = true
-				last_decision_lane = 1
+		elif chosen_lane == 1:
+			surf_b = Surface.CEILING if surf_b == Surface.FLOOR else Surface.FLOOR
+			mag_b = true
 
+		last_lane = chosen_lane
 		points.append(TrajectoryPoint.new(current_x, surf_a, surf_b, swap_trigger, mag_a, mag_b))
 		current_x += interval
 
